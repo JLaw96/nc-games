7dfc2f2f580bdad4361c54f0b89b952bd51476e1
jonathan <jonathan@Jonathan>
2023-03-14 14:44:25 +0000

    added loading state

--- src/components/reviews/Reviews.jsx
+++ src/components/reviews/Reviews.jsx
@@ -3,29 +3,34 @@ import getReviews from "../utils/api's";
 
 const Reviews = () => {
   const [reviews, setReviews] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     getReviews().then((reviewsFromApi) => {
       setReviews(reviewsFromApi);
+      setIsLoading(false);
     });
   }, []);
-
-  return (
-    <main>
-      <h1>Reviews</h1>
-      <ul>
-        {reviews.map((review) => {
-          return (
-            <li key={review.review_id}>
-              <h2>Title: {review.title}</h2>
-              <h3>Votes: {review.votes}</h3>
-              <img src={review.review_img_url} alt={review.title}></img>
-            </li>
-          );
-        })}
-      </ul>
-    </main>
-  );
+  if (isLoading) {
+    return <p>Loading...</p>;
+  } else {
+    return (
+      <main>
+        <h1>Reviews</h1>
+        <ul>
+          {reviews.map((review) => {
+            return (
+              <li key={review.review_id}>
+                <h2>Title: {review.title}</h2>
+                <h3>Votes: {review.votes}</h3>
+                <img src={review.review_img_url} alt={review.title}></img>
+              </li>
+            );
+          })}
+        </ul>
+      </main>
+    );
+  }
 };
 
 export default Reviews;
